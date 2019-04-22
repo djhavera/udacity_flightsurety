a272b860992476817a982cf89045c3bd7af0dbac
David Havera <djhavera@yahoo.com>
2019-04-22 11:03:00 -0400

    Add files via upload

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,5 @@
 import FlightSuretyApp from '../../build/contracts/FlightSuretyApp.json';
+import FlightSuretyData from '../../build/contracts/FlightSuretyData.json';
 import Config from './config.json';
 import Web3 from 'web3';
 import express from 'express';
@@ -8,22 +9,89 @@ let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 let flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
+let flightSuretyData = new web3.eth.Contract(FlightSuretyData.abi, config.dataAddress);
 
+let oracle_accounts = [];
 
-flightSuretyApp.events.OracleRequest({
-    fromBlock: 0
-  }, function (error, event) {
-    if (error) console.log(error)
-    console.log(event)
+const STATUS_CODE_UNKNOWN = 0;
+const STATUS_CODE_ON_TIME = 10;
+const STATUS_CODE_LATE_AIRLINE = 20;
+const STATUS_CODE_LATE_WEATHER = 30;
+const STATUS_CODE_LATE_TECHNICAL = 40;
+const STATUS_CODE_LATE_OTHER = 50;
+
+async function test() {
+  let fee = await flightSuretyApp.methods.REGISTRATION_FEE().call();
+  let accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  accounts.forEach(async account => {
+      let r = await flightSuretyApp.methods.registerOracle().send({
+          "from": account,
+          "value": fee,
+          "gas": 47123880000,
+          "gasPrice": 100000
+      });
+      let result = await flightSuretyApp.methods.getMyIndexes.call({from: account});
+      //console.log(result);
+      console.log(`Oracle ${account} registered: ${result[0]}, ${result[1]}, ${result[2]}`);
+  });
+}
+
+test();
+flightSuretyApp.events.OracleRequest({ 
+  fromBlock: "latest" }, async function (error, event) { 
+      if (error) { 
+          console.log(error); 
+      } 
+
+  let airline = event.returnValues.airline; 
+  let flight = event.returnValues.flight;
+  let timestamp = event.returnValues.timestamp; 
+  let found = false;
+
+  let selectedCode = STATUS_CODES[1];
+  let scheduledTime = (timestamp * 1000);
+  console.log(`Flight scheduled to: ${new Date(scheduledTime)}`);
+  if (scheduledTime < Date.now()) {
+      selectedCode = STATUS_CODES[2];
+  }
+
+  oracles.forEach((oracle, index) => {
+      if (found) {
+          return false;
+      }
+      for(let idx = 0; idx < 3; idx += 1) {
+          if (found) {
+              break;
+          }
+          if (selectedCode.code === 20) {
+              console.log("WILL COVER USERS");
+              flightSuretyApp.methods.creditInsurees(
+                  accounts[index],
+                  flight
+              ).send({
+                  from: accounts[index]
+              }).then(result => {
+                  //console.log(result);
+                  console.log(`Flight ${flight} got covered and insured the users`);
+              }).catch(err => {
+                  console.log(err.message);
+              });
+          }
+          flightSuretyApp.methods.submitOracleResponse(
+              oracle[idx], airline, flight, timestamp, selectedCode.code
+          ).send({
+              from: accounts[index]
+          }).then(result => {
+              found = true;
+              console.log(`Oracle: ${oracle[idx]} responded from flight ${flight} with status ${selectedCode.code} - ${selectedCode.label}`);
+          }).catch(err => {
+              console.log(err.message);
+          });
+      }
+  });
 });
 
-const app = express();
-app.get('/api', (req, res) => {
-    res.send({
-      message: 'An API for use with your Dapp!'
-    })
-})
+const app = express(); app.get('/api', (req, res) => { res.send({ message: 'An API for use with your Dapp!' }); });
 
 export default app;
-
-
