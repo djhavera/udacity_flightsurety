9fdf33128531d85b425c37a849a9c51223cdddef
David Havera <djhavera@yahoo.com>
2019-04-27 13:04:09 -0400

    Add files via upload

--- test/flightSurety.js
+++ test/flightSurety.js
@@ -3,7 +3,7 @@ var BigNumber = require('bignumber.js');
 
 var Web3Utils = require('web3-utils');
 var Web3 = require('web3')
-web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
 
 contract('Flight Surety Tests', async (accounts) => {
 
@@ -96,6 +96,63 @@ contract('Flight Surety Tests', async (accounts) => {
     assert.equal(result, false, "Airline should not be able to register another airline if it hasn't provided funding");
 
   });
- 
+
+  
+  it('(multiparty) only existing airline may register a new airline until there are at least four airlines registered', async () => {
+
+    // ARRANGE
+    let newAirline = accounts[2];
+
+    let funds = 10000000000000000000; // 10 ether
+
+    try{
+      await config.flightSuretyApp.registerAirlineFunction(newAirline, {from: config.firstAirline});
+    }
+    catch(e) {
+
+    }
+    let result = await config.flightSuretyData.isAirlineRegisteredFunction(newAirline);
+
+    await config.flightSuretyApp.sendFundToAirline(config.firstAirline, {from: config.firstAirline, value: funds}); 
+    await config.flightSuretyApp.registerAirlineFunction(newAirline, {from: config.firstAirline});
+    let result2 = await config.flightSuretyData.isAirlineRegisteredFunction(newAirline);
+
+    assert.equal(result, false, "Airline was registered but it should not.");
+    assert.equal(result2, true, "Airline was not registered but it should.");
+
+    });
+    it('(multiparty) registration of fifth and subsequent airlines requires multi-party consensus of 50% of registered airliner', async () => {
+
+        // ARRANGE
+        let newAirline = accounts[3];
+        let newAirline2 = accounts[4];
+        let newAirline3 = accounts[5];
+        let newAirline4 = accounts[6];
+        let registerBelowThreshold = true;
+        let funds = 10000000000000000000; // 10 ether
+        try{
+        await config.flightSuretyApp.registerAirlineFunction(newAirline2, {from: config.firstAirline});
+        //await config.flightSuretyApp.sendFundToAirline(newAirline2, {from: config.firstAirline, value: funds}); 
+        await config.flightSuretyApp.registerAirlineFunction(newAirline3, {from: config.firstAirline});
+        await config.flightSuretyApp.registerAirlineFunction(newAirline4, {from: config.firstAirline});
+        //await config.flightSuretyApp.sendFundToAirline(newAirline4, {from: config.firstAirline, value: funds});
+        }
+        catch(e) {
+            registerBelowThreshold = false;
+            console.log(e);
+        }
+        let result = await config.flightSuretyData.isAirlineRegisteredFunction.call(newAirline4);
+        // ASSERT
+        //assert.equal(registerBelowThreshold, true, "Can not register Airlines but should work");
+        //assert.equal(await config.flightSuretyApp.airlinesRegisteredCount.call(), 4, "Threshold ignored");
+    
+        assert.equal(result, true, "Airline was not registered.");
+
+        
+    });
+    
+
+
+  
 
 });
